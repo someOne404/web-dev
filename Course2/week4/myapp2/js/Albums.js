e606e318277d0dc38dc0446928ac305d1f27087a
Bonus <jiayilu96@gmail.com>
2019-08-05 20:21:36 -0400

    Course 2 done

---
+++ Course2/week4/myapp2/js/Albums.js
@@ -0,0 +1,99 @@
+// this file contains the data we need for the gallery
+// The main object, "gallery" contains an array of album
+// each album contains an array of photos 
+// (plus a name and a thumbnail image)
+// The photos contain an image src and some metadata
+
+var gallery = {
+	albums : [
+		{
+			name : "Bona", 
+			thumbnail : "images/1.jpg",
+			photos : [
+				{	
+					src : "images/1.jpg",
+					title : "Bona", 
+					description : "Bona"
+				}, 
+				{	
+					src : "images/2.jpg",
+					title : "Bona", 
+					description : "Bona"
+				}, 
+				{	
+					src : "images/3.jpg",
+					title : "Bona", 
+					description : "Bona"
+				}, 
+				{	
+					src : "images/4.jpg",
+					title : "Bona", 
+					description : "Bona"
+				},
+				{
+					src : "images/5.jpeg",
+					title : "Bona", 
+					description : "Bona"
+				},
+				{
+					src : "images/6.jpeg",
+					title : "Bona", 
+					description : "Bona"
+				}
+			]
+		},
+		{
+			name : "Irene", 
+			thumbnail : "images/bae_1.jpg",
+			photos : [
+				{	
+					src : "images/bae_1.jpg",
+					title : "Irene", 
+					description : "Irene"
+				}, 
+				{	
+					src : "images/bae_2.jpg",
+					title : "Irene", 
+					description : "Irene"
+				}, 
+				{	
+					src : "images/bae_3.jpg",
+					title : "Irene", 
+					description : "Irene"
+				},
+				{
+					src : "images/bae_4.jpg",
+					title : "Irene", 
+					description : "Irene"
+				}  
+			]
+		},
+		{
+			name : "Krystal", 
+			thumbnail : "images/krys_1.jpeg",
+			photos : [
+				{	
+					src : "images/krys_1.jpeg",
+					title : "Krystal", 
+					description : "Krystal"
+				}, 
+				{	
+					src : "images/krys_2.jpeg",
+					title : "Krystal", 
+					description : "Krystal"
+				}, 
+				{	
+					src : "images/krys_3.jpg",
+					title : "Krystal", 
+					description : "Krystal"
+				},
+				{
+					src : "images/krys_4.jpg",
+					title : "Krystal", 
+					description : "Krystal"
+				} 
+			]
+		}
+
+	]
+};
